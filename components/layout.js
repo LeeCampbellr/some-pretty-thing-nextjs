a3942bdaf137c07b3d3b8a7a9e13760c6256e12f
Lee Campbell <leecampbellr@gmail.com>
2022-01-16 10:38:05 -0500

    adding back navigation

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@components/navigation/navigation"
 export default function Layout({ children }) {
   return (
     <>
+      <Navigation />
       <main>{children}</main>
     </>
   )
